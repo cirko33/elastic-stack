e09f242ba94377c8dedee619bfd18c13d78e5c97
nikola-vukic-alt <vukic.e268.2023@uns.ac.rs>
2024-04-01 20:22:48 +0200

    thefts on street count

--- elasticsearch-client/client.py
+++ elasticsearch-client/client.py
@@ -7,43 +7,69 @@ client = Elasticsearch(
 )
 
 index = "logs-generic-default"
-target_index = "narcotics-count-per-year"
+# region narcotics
 
-query = {
-    "size": 0,
-    "aggs": {
-        "years": {
-            "terms": {
-                "field": "Year",
-            },
-            "aggs": {
-                "narcotics_count": {
-                    "filter": {
-                        "term": {
-                            "Primary Type": "NARCOTICS"
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+# target_index = "narcotics-count-per-year"
 
-response = client.search(index=index, body=query)
+# query = {
+#     "size": 0,
+#     "aggs": {
+#         "years": {
+#             "terms": {
+#                 "field": "Year",
+#             },
+#             "aggs": {
+#                 "narcotics_count": {
+#                     "filter": {
+#                         "term": {
+#                             "Primary Type": "NARCOTICS"
+#                         }
+#                     }
+#                 }
+#             }
+#         }
+#     }
+# }
 
-buckets = response["aggregations"]["years"]["buckets"]
+# response = client.search(index=index, body=query)
 
-documents = []
-for bucket in buckets:
-    year = bucket["key"]
-    narcotics_count = bucket["narcotics_count"]["doc_count"]
-    doc = {
-        "Year": year,
-        "NarcoticsCount": narcotics_count
-    }
-    documents.append(doc)
+# buckets = response["aggregations"]["years"]["buckets"]
 
-for doc in documents:
-    client.index(index=target_index, body=doc)
+# documents = []
+# for bucket in buckets:
+#     year = bucket["key"]
+#     narcotics_count = bucket["narcotics_count"]["doc_count"]
+#     doc = {
+#         "Year": year,
+#         "NarcoticsCount": narcotics_count
+#     }
+#     documents.append(doc)
 
-print(f"Documents indexed to '{target_index}' index.")
+# for doc in documents:
+#     client.index(index=target_index, body=doc)
+
+# print(f"Documents indexed to '{target_index}' index.")
+# endregion narcotics 
+
+index = "logs-generic-default"
+
+# Define the query
+# query = {
+#     "query": {
+#         "bool": {
+#             "must": [
+#                 { "match": { "Location Description": "STREET" } },
+#                 { "match": { "Primary Type": "THEFT" } }
+#             ]
+#         }
+#     }
+# }
+
+# response = client.count(index=index, body=query)
+
+# # Get the count
+# theft_count = response["count"]
+
+# print(f"Number of thefts that happened on the street: {theft_count}")
+
+# endregion theft
